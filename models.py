fccd22ab37a269ae9ce131379a741906ec272494
Gabriele Facciolo <gfacciol@gmail.com>
2018-04-14 11:08:13 +0200

    add matlab pretrained weights and color model

--- models.py
+++ models.py
@@ -23,3 +23,22 @@ def DnCNN():
     model = Model(inputs=inpt, outputs=x)
     
     return model
+
+
+def DnCNN_C():
+    
+    inpt = Input(shape=(None,None,3))
+    # 1st layer, Conv+relu
+    x = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same')(inpt)
+    x = Activation('relu')(x)
+    # 15 layers, Conv+BN+relu
+    for i in range(18):
+        x = Conv2D(filters=64, kernel_size=(3,3), strides=(1,1), padding='same')(x)
+        x = BatchNormalization(axis=-1, epsilon=1e-3)(x)
+        x = Activation('relu')(x)   
+    # last layer, Conv
+    x = Conv2D(filters=3, kernel_size=(3,3), strides=(1,1), padding='same')(x)
+    x = Subtract()([inpt, x])   # input - noise
+    model = Model(inputs=inpt, outputs=x)
+    
+    return model
